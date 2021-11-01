c45582327a87863cd1d9745082a340b88992f150
Gary <williamsg12>
2021-11-01 08:45:46 -0400

    added stuff

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,13 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'
 import API_URL  from '../../config';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
 
  
 
     return (
-        <div>
+        <div className='container'>
+            <div className='mt-5 p-5 bg-light'>
+                <h1 className='display-4'>Welcome to postland </h1>
+                <p className='lead'>A fullstack application that uses react and django</p>
+                <hr className='my-4' />
+                <p>Please Log in</p>
+                <Link to='/login' className='btn btn-dark btn-lg' >Login</Link>
+            </div>
             
         </div>
     );
