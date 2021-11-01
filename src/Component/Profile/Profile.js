07c091b5bccd9d8643e92aa6cd531abcf9034871
Gary <williamsg12>
2021-11-01 08:54:55 -0400

    added stuff

--- src/Component/Profile/Profile.js
+++ src/Component/Profile/Profile.js
@@ -1,34 +1,37 @@
 import axios from 'axios';
-import React, { useEffect,useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import URL from '../../config';
-const Profile = () => {
-    const [state, setstate] = useState({})
-    const tokens = `c096b0cab2c6cc47dabe4fdbb3f5bd5307619a1e`;
-    const getProfile= async()=>{
-        try {
-            const response = await axios
-							.get(`http://localhost:8000/users/`, {
-								headers: { Authorization: `Bearer ${tokens}` },
-							})
-							.then(setstate(response.data));
-        } catch (error) {
-            
-        }
-    }
-    useEffect(() => {
-       getProfile()
-    }, [])
-    return (
-        <div>
-            <h1>Profile Details</h1>
+import './Profile.css'
 
-            <h3>Email:{state.email}</h3>
-            <h3>Id:{state.id}</h3>
-            <h3>Username:{state.username}</h3>
-            
-        
-        </div>
-    );
+const Profile = () => {
+	const [state, setstate] = useState({});
+	const tokens = `c096b0cab2c6cc47dabe4fdbb3f5bd5307619a1e`;
+	const getProfile = async () => {
+		try {
+			const response = await axios
+				.get(`http://localhost:8000/users/`, {
+					headers: { Authorization: `Bearer ${tokens}` },
+				})
+				.then(setstate(response.data));
+		} catch (error) {}
+	};
+	useEffect(() => {
+		getProfile();
+	}, []);
+	return (
+		<div className='container'>
+			<div className='mt-5 p-5 bg-light main'>
+				<h1 className='head'>Profile Details</h1>
+
+				<h3>Email: {state.email}</h3>
+				<h3>Id: {state.id}</h3>
+				<h3>Username: {state.username}</h3>
+                <h3>Name:</h3>
+                <h3>Age:</h3>
+                <h3>Gender:</h3>
+			</div>
+		</div>
+	);
 };
 
 export default Profile;
