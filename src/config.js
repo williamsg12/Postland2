accd3749c45c86eb2ca7d02cbd1ce1003d607499
Gary <williamsg12>
2021-11-02 13:40:06 -0400

    added stuff

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const URL =
 	window.location.hostname === 'localhost'
 		? 'http://localhost:8000'
-		: 'https://grim-spider-92723.herokuapp.com';
+		: 'http://grim-spider-92723.herokuapp.com';
 
 export default URL;
